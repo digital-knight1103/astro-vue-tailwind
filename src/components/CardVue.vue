997e655b45163230db3bef2917b2965fbd34c2d0
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-04-28 11:42:21 +0200

    text VUE component

---
+++ src/components/CardVue.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-96 h-96 bg-slate-200">
+    <h1 class="text-xl font-bold">{{ title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CardVue',
+  props: {
+    title: {
+      type: String,
+      default: 'Test'
+    }
+  },
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
