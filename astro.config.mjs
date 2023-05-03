2db71c6662034f179abde9b944842e98fd0ffcf4
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-05-03 08:16:47 +0200

    Add netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
+// import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/edge-functions';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
